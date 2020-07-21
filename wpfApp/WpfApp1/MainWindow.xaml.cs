6d3e6391cde82fc527b060ede1278c029d40c701
yinghangy <yinghang_yang@yahoo.com>
2020-07-21 11:37:49 -0700

    Add WpfApp folder

---
+++ wpfApp/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string a = "(M+H4N)+";
+            string b = "(M+NH4H)+";
+            var aSet = new HashSet<char>(a);
+            var bSet = new HashSet<char>(b);
+            bool abSame = aSet.SetEquals(b);
+            int o = 7;
+            string s = "(M+H4N)+";
+            string s1 = "(M+H4N)+";
+            var dd = (s1.Count() == s.Count()) && !s1.Except(s).Any();
+           
+        }
+    }
+}
